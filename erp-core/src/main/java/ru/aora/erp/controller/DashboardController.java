d71db200dc43f458abc0e245beaae0d03af29b1a
kozmo <kozmod@bk.ru>
2019-09-10 20:49:39 +0300

    fix: create GarantResultController and template

--- erp-core/src/main/java/ru/aora/erp/controller/DashboardController.java
+++ erp-core/src/main/java/ru/aora/erp/controller/DashboardController.java
@@ -3,60 +3,31 @@ package ru.aora.erp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.aora.erp.entity.dto.compose.KsContractCounteragentCollectionDto;
-import ru.aora.erp.entity.dto.compose.KsContractCounteragentDto;
-import ru.aora.erp.service.ContractService;
-import ru.aora.erp.service.CounteragentService;
-import ru.aora.erp.service.KsService;
 import ru.aora.erp.service.SidebarModulesIdentifiersService;
 
-import java.util.List;
 import java.util.Map;
 
-import static ru.aora.erp.entity.dto.compose.KsContractCounteragentDtoUtils.sortByGarantDateNaturalOrder;
-import static ru.aora.erp.entity.dto.compose.KsContractCounteragentDtoUtils.toKsContractCounteragentDtoList;
 import static ru.aora.erp.controller.SecurityController.ROOT_MAPPING;
 import static ru.aora.erp.utils.mvc.MvcUtils.redirectTo;
 
 
 @Controller
-
 public final class DashboardController {
 
     public static final String DASHBOARD_MAPPING = "/dashboard";
+
     private static final String DASHBOARD_TEMPLATE = "dashboard";
     private static final String UI_CHANE_NODE_MODEL = "uiChaneNodeModel";
-    private static final String DTO_MODEL = "ksSortDto";
-
-    private KsService ksService;
-    private ContractService contractService;
-    private CounteragentService counteragentService;
-
 
     private final SidebarModulesIdentifiersService modulesIdentifiersService;
 
     @Autowired
-
-    public DashboardController(SidebarModulesIdentifiersService modulesIdentifiersService,
-                               KsService ksService,
-                               ContractService contractService,
-                               CounteragentService counteragentService) {
+    public DashboardController(SidebarModulesIdentifiersService modulesIdentifiersService) {
         this.modulesIdentifiersService = modulesIdentifiersService;
-        this.ksService = ksService;
-        this.contractService = contractService;
-        this.counteragentService = counteragentService;
     }
 
     @RequestMapping(DASHBOARD_MAPPING)
     public String dashboard(Map<String, Object> model) {
-        final List<KsContractCounteragentDto> ksDtoList = sortByGarantDateNaturalOrder(
-                toKsContractCounteragentDtoList(
-                        ksService.loadAll(),
-                        contractService.loadAll(),
-                        counteragentService.loadAll())
-        );
-        model.put(DTO_MODEL, KsContractCounteragentCollectionDto.of(ksDtoList));
-
         model.put(
                 UI_CHANE_NODE_MODEL,
                 modulesIdentifiersService.loadAll()
@@ -68,5 +39,4 @@ public final class DashboardController {
     public String redirectToRoot() {
         return redirectTo(DASHBOARD_TEMPLATE);
     }
-
 }
